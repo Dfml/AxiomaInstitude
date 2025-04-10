ed5af429bb62336245d4003c14e1a6c477cf5580
Daniel Felipe Mendez Lopez <danifelipemendez@gmail.com>
2025-04-10 17:00:19 -0500

    Tailwind config

--- src/app/page.tsx
+++ src/app/page.tsx
@@ -2,7 +2,7 @@ import Image from "next/image";
 
 export default function Home() {
   return (
-    <div className=" flex w-full h-screen">
+    <div className=" flex w-full h-screen bg-putty-900 ">
       
     </div>
   );
