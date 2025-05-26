8a8ec716c8203e508ea41db31767c3dacac669ed
Daniel Felipe Mendez Lopez <danifelipemendez@gmail.com>
2025-05-26 16:11:19 -0500

    fixed children error

--- src/app/components/CardPlusButton.tsx
+++ src/app/components/CardPlusButton.tsx
@@ -11,9 +11,8 @@ interface CardsPlusButton {
 export default function Cards( ) {
   return (
     <>
-   <button className="w-90 h-80 bg-putty-50 rounded-xl text-tangaroa-950 flex items-center justify-center  shadow-lg  transition-all hover:text-putty-50 hover:bg-tangaroa-950"><Plus size={150}></Plus> </button>
-    {Children}
-      </>
+    <button className="w-90 h-80 bg-putty-50 rounded-xl text-tangaroa-950 flex items-center justify-center  shadow-lg  transition-all hover:text-putty-50 hover:bg-tangaroa-950"><Plus size={150}></Plus> </button>
+    </>
 
   );
 }
